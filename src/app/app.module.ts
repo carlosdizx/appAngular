a45c6aac236abdc1097697b3ac3459958d1d458a
Carlos Diaz <carlos.diaz@ias.com.co>
2022-07-21 22:26:42 -0500

    instaldo angular material y referenciado desde un nuevo modulo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {FormsModule} from "@angular/forms";
+import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from './shared/components/header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from '../material.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent
+  declarations: [AppComponent, HeaderComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    MaterialModule,
+    BrowserAnimationsModule,
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        FormsModule
-    ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
