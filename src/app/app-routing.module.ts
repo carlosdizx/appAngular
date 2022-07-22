3cb42b107b1e3df9c4f97047504f8599bd861ef4
Carlos Diaz <carlos.diaz@ias.com.co>
2022-07-21 22:47:51 -0500

    creado componente y bindiado a una ruta

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DominicodeComponent } from './pages/dominicode/dominicode.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'dominicode', component: DominicodeComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/pages/dominicode/dominicode.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dominicode',
+  templateUrl: './dominicode.component.html',
+})
+export class DominicodeComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
