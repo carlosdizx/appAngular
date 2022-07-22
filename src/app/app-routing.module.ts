e3d173c26d7f16ad3748e45291e6eb9dcfc9178e
Carlos Diaz <carlos.diaz@ias.com.co>
2022-07-21 22:55:06 -0500

    redirigiendo al home en caso de no encontrar una ruta que este en el listado de Rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DominicodeComponent } from './pages/dominicode/dominicode.component';
 
-const routes: Routes = [{ path: 'dominicode', component: DominicodeComponent }];
+const routes: Routes = [
+  { path: 'dominicode', component: DominicodeComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
